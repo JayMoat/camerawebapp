283eb60f2fa7798785a9d8713ed7be04293aa945
JayMoat <78933810+JayMoat@users.noreply.github.com>
2021-02-12 09:37:01 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -5,28 +5,7 @@ const cameraView = document.querySelector("#camera--view"),
       cameraSensor = document.querySelector("#camera--sensor"),
       cameraTrigger = document.querySelector("#camera--trigger")
 
-function download(){
-    var download = document.getElementById("download");
-    var image = document.getElementById("CANVAS").toDataURL("image/png")
-                    .replace("image/jpg", "image/octet-stream");
-        download.setAttribute("href", image);
-        //download.setAttribute("download","archive.png");
-  }
-var i=0;
-function myFunction() {
-  var x =  document.getElementById("CANVAS") ;
-  var ctx = x.getContext("2d");
-  ctx.fillStyle = "#FF0000";
-  
-  ctx.drawImage(video, 0, 0, 400, 350);
 
-  //ctx.fillRect(20, 20, 150, 100);
-
-  if (i <10)
-  {
-  document.body.appendChild(x);
-  i=i+1;
-};
 
 function cameraStart() {
     navigator.mediaDevices
@@ -48,5 +27,4 @@ cameraTrigger.onclick = function() {
     cameraOutput.classList.add("taken");
 };
 window.addEventListener("load", cameraStart, false);
-window.addEventListener("load", download, false);
-window.addEventListener("load", myFunction, false);
+
